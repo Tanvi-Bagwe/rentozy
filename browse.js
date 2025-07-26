28dfe1bb50d4b2b205c757e085844699b72587f9
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2025-07-26 14:36:39 +0100

    Added list of houses

---
+++ browse.js
@@ -0,0 +1,20 @@
+const house = [
+    {
+        title: "2 BHK in Dublin",
+        location: "Dublin",
+        price: 2500,
+        discription: "Spacious 2BHK Mid terrace house near city centre."
+    },
+    {
+        title: "2 BHK in Limerick",
+        location: "Limerick",
+        price: 1200,
+        discription: "2 BHK semi-detached house in limerick."
+    },
+    {
+        title: "3 BHK in Cork",
+        location: "Cork",
+        price: 2500,
+        discription: "Beautiful 3 BHK detached house near city."
+    },
+]
